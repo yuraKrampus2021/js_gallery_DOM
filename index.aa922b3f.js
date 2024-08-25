412e93517da7eb6e64a46f9c7ce70639f221662f
Yurii Kramar <y.kramar15@gmail.com>
2024-08-25 16:13:37 +0200

    make build

---
+++ index.aa922b3f.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.querySelector("#largeImg");e.addEventListener("click",function(e){e.preventDefault(),t.src=e.target.parentElement.href});
+//# sourceMappingURL=index.aa922b3f.js.map
